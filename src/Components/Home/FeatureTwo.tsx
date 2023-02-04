c6ff815a23c061f52c9ccbce3f091cc3c32a2ed1
Sandipan Das <87384281+sandidas@users.noreply.github.com>
2023-02-04 13:49:17 +0530

    Icons are updated

--- src/Components/Home/FeatureTwo.tsx
+++ src/Components/Home/FeatureTwo.tsx
@@ -2,6 +2,13 @@ import { Button } from "@mantine/core";
 import React from "react";
 import BusinessHosting from "../../../public/images/nav/businessHosting.svg";
 
+import HostingServicesIcon from "../../../public/images/website/hostingServices.svg";
+import DomainIcon from "../../../public/images/website/domainIcon.svg";
+import EmailIcon from "../../../public/images/website/emailIcon.svg";
+import SecurityServicesIcon from "../../../public/images/website/securityServicesIcon.svg";
+import WebAppSolutionsIcon from "../../../public/images/website/webAppSolutions.svg";
+import WordPressSolutionsIcon from "../../../public/images/website/wordPressSolutions.svg";
+
 const FeatureTwo = () => {
   return (
     <>
@@ -14,7 +21,7 @@ const FeatureTwo = () => {
           
           <div className="bg-surface p-10 rounded-lg">
             <div className="space-y-3 md:min-h-[270px]">
-              <BusinessHosting className="fill-primary w-20 h-20" />
+              <HostingServicesIcon className="fill-primary w-14 h-14" />
               <h2 className="text-text">Hosting Services</h2>
               <p className="text-text">Get access to enterprise-grade web hosting for businesses, including WordPress hosting, eCommerce hosting, VPS hosting, reseller (WHMCS) hosting, and more.</p>
             </div>
@@ -22,7 +29,7 @@ const FeatureTwo = () => {
 
           <div className="bg-surface p-10 rounded-lg">
             <div className="space-y-3 md:min-h-[270px]">
-              <BusinessHosting className="fill-primary w-20 h-20" />
+              <WordPressSolutionsIcon className="fill-primary w-14 h-14" />
               <h2 className="text-text">WordPress Solutions</h2>
               <p className="text-text">Keep your site up-to-date and secure for your online business. Our WordPress solutions are specifically designed to optimize the performance of your website.</p>
             </div>
@@ -30,7 +37,7 @@ const FeatureTwo = () => {
 
           <div className="bg-surface p-10 rounded-lg">
             <div className="space-y-3 md:min-h-[270px]">
-              <BusinessHosting className="fill-primary w-20 h-20" />
+              <WebAppSolutionsIcon className="fill-primary w-14 h-14" />
               <h2 className="text-text">Web App Solutions</h2>
               <p className="text-text">Build and deploy your web apps without managing any infrastructure. A speedy, global CDN automatically backs our web app hosting with optimal security out of the box.</p>
             </div>
@@ -39,7 +46,7 @@ const FeatureTwo = () => {
 
           <div className="bg-surface p-10 rounded-lg">
             <div className="space-y-3 md:min-h-[270px]">
-              <BusinessHosting className="fill-primary w-20 h-20" />
+              <SecurityServicesIcon className="fill-primary w-14 h-14" />
               <h2 className="text-text">Security Services</h2>
               <p className="text-text">Don’t leave your business exposed to vulnerabilities. Improve customer confidence, secure your data, and boost your SEO ranking with an SSL certificate to encrypt traffic to and from your website.</p>
             </div>
@@ -48,7 +55,7 @@ const FeatureTwo = () => {
 
           <div className="bg-surface p-10 rounded-lg">
             <div className="space-y-3 md:min-h-[270px]">
-              <BusinessHosting className="fill-primary w-20 h-20" />
+              <EmailIcon className="fill-primary w-14 h-14" />
               <h2 className="text-text">Business Email Services</h2>
               <p className="text-text">Make sure you never miss a message. Our purpose-built hosted business email solutions are equipped with spam and virus filtering.</p>
             </div>
@@ -57,7 +64,7 @@ const FeatureTwo = () => {
 
           <div className="bg-surface p-10 rounded-lg">
             <div className="space-y-3 md:min-h-[270px]">
-              <BusinessHosting className="fill-primary w-20 h-20" />
+              <DomainIcon className="fill-primary w-14 h-14" />
               <h2 className="text-text">Domains</h2>
               <p className="text-text">Take the initial step to setting up your digital presence with the right domain name. We also help you protect your identity online and retain full control of your domain.</p>
             </div>
