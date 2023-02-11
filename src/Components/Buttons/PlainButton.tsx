d1eb4576fe517da255169a84e66c28b6649346af
Sandipan Das <87384281+sandidas@users.noreply.github.com>
2023-02-12 00:27:09 +0530

    New Updated

--- src/Components/Buttons/PlainButton.tsx
+++ src/Components/Buttons/PlainButton.tsx
@@ -9,12 +9,12 @@ interface IButton{
 const PlainButton = ({text, handler}:IButton) => {
     return (
         <button onClick={handler}
-        className="transition ease-in-out duration-500 flex h-10 items-center font-medium group-hover:text-primary"
+        className="transition ease-in-out duration-500 flex h-10 items-center font-medium text-primary hover:animate-pulse "
         >
             <span className='tracking-normal group-hover:tracking-wider duration-300 border-b-2 hover:border-primary'>
             {text && text}
             </span>
-            <ArrowSmallRightIcon className="h-8 w-8 text-primary pl-2 group-hover:translate-x-2 duration-300" />
+            <ArrowSmallRightIcon className="h-8 w-8  pl-2 group-hover:translate-x-2 duration-300" />
         </button>
     );
 };
