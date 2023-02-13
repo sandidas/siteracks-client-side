84595e31acd9baca7b479cc8d9345781d4c45915
Sandipan Das <87384281+sandidas@users.noreply.github.com>
2023-02-13 23:40:45 +0530

    wEB hOSTING pAGE ONGOING

--- src/Components/Pages/WebHosting/WebHostingEssentials.tsx
+++ src/Components/Pages/WebHosting/WebHostingEssentials.tsx
@@ -5,6 +5,7 @@ import LatestTechIcon from "@/Components/Images/web-hosting/latestTechIcon.svg";
 import PlainButton from "@/Components/Buttons/PlainButton";
 import { useModalContext } from "@/Context/ModalProvider";
 import WHBlazingFast from "./SubCompo/WHBlazingFast";
+import WHEasyAndUserFriendly from "./SubCompo/WHEasyAndUserFriendly";
 const WebHostingEssentials = () => {
     // use modal context 
     const { requestModal, setModalData, setModalTitle } = useModalContext();
@@ -38,21 +39,21 @@ const WebHostingEssentials = () => {
         <FeatureCard>
           <div className="space-y-5">
             <LatestTechIcon className="fill-primary w-14 h-14" />
-            <h3 className="text-title font-bold text-3xl">Blazing fast websites</h3>
-            <p className="text-text">Our servers are designed for optimal performance, utilizing the infrastructure of Digital Ocean and AWS, and incorporating several custom speed enhancements.</p>
+            <h3 className="text-title font-bold text-3xl">Easy and User-Friendly Client Interfaces</h3>
+            <p className="text-text">Our plans offer hassle-free One-click WordPress installation and our exclusive. Enjoy managed auto-updates and expert support, plus added benefits.</p>
           </div>
           <PlainButton
                 text="LEARN MORE"
                 handler={() => {
-                  setModalTitle("Blazing fast websites"),
-                  setModalData(<WHBlazingFast />),
+                  setModalTitle("Easy and User-Friendly Client Interfaces"),
+                  setModalData(<WHEasyAndUserFriendly />),
                   requestModal();
                 }}
               />
         </FeatureCard>
 
 
-
+{/* WORK ON PROGRESS HERE  */}
 
 
         <FeatureCard>
