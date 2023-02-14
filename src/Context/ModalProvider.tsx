5445a1c06955612bcdec8673e14270ffd91f316d
Sandipan Das <87384281+sandidas@users.noreply.github.com>
2023-02-14 22:06:38 +0530

    Modal is little updated

--- src/Context/ModalProvider.tsx
+++ src/Context/ModalProvider.tsx
@@ -1,7 +1,8 @@
 "use client"; // this is a client component
+import PlainButton from "@/Components/Buttons/PlainButton";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 import { Modal } from "@mantine/core";
-import React, {Dispatch,SetStateAction, createContext, FC, useContext, useState } from "react";
+import React, { Dispatch, SetStateAction, createContext, FC, useContext, useState } from "react";
 
 interface MyProps {
   children?: React.ReactNode;
@@ -12,40 +13,33 @@ interface MyProps {
 // ================
 interface IModalProps {
   //Dispatch<SetStateAction<any | boolean>>
-    setModalData?: any | null ;
-    setModalTitle?: any | null ;
-    requestModal():  void;
-  }
-  export const ModalContext = createContext<IModalProps>({
-    setModalData: null,
-    setModalTitle: null,
-    requestModal: () => {}, // define a function
-  });
-
-
-
+  setModalData?: any | null;
+  setModalTitle?: any | null;
+  requestModal(): void;
+}
+export const ModalContext = createContext<IModalProps>({
+  setModalData: null,
+  setModalTitle: null,
+  requestModal: () => {}, // define a function
+});
 
 export const ModalProvider: React.FC<MyProps> = ({ children }) => {
+  const [modalOpened, setModalOpened] = useState<boolean>(false);
+  const [modalData, setModalData] = useState<any | null>(null);
+  const [modalTitle, setModalTitle] = useState<any | null>(null);
 
-    const [modalOpened, setModalOpened] = useState<boolean>(false);
-    const [modalData, setModalData] = useState<any | null>(null);
-    const [modalTitle, setModalTitle] = useState<any | null>(null);
-
-    const requestModal = () => {
-        setModalOpened(true);
-    };
-    // console.log("modalTitle", modalTitle);
-    // console.log("modalOpened", modalOpened);
-    // console.log("modalData", modalData);
+  const requestModal = () => {
+    setModalOpened(true);
+  };
+  // console.log("modalTitle", modalTitle);
+  // console.log("modalOpened", modalOpened);
+  // console.log("modalData", modalData);
 
   return (
     <>
-    <ModalContext.Provider value={{setModalData,  setModalTitle, requestModal}}>
-      {children}
-    </ModalContext.Provider>
+      <ModalContext.Provider value={{ setModalData, setModalTitle, requestModal }}>{children}</ModalContext.Provider>
 
-
-    <Modal
+      <Modal
         radius="xl"
         zIndex="99999"
         padding={0}
@@ -57,6 +51,7 @@ export const ModalProvider: React.FC<MyProps> = ({ children }) => {
         }}
         withCloseButton={false}
         style={{ marginTop: "20px" }}
+        className="group"
       >
         {/* Modal content */}
         <div className="text-title font-bold text-center text-2xl p-3 lg:p-10 space-x-2 flex w-full justify-between items-center rounded-3xl">
@@ -73,8 +68,16 @@ export const ModalProvider: React.FC<MyProps> = ({ children }) => {
         </div>
 
         <div className="p-3 lg:p-10 bg-surface">{modalData && modalData}</div>
+
+        <div className="flex flex-col py-5 items-center">
+          <PlainButton
+            text="Close"
+            handler={() => {
+              setModalOpened(false), setModalData(null), setModalTitle(null);
+            }}
+          />
+        </div>
       </Modal>
-    
     </>
   );
 };
