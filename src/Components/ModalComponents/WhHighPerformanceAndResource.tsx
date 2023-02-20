873a543c1c3e21277df4cb0f098626d6d17be5d5
Sandipan Das <87384281+sandidas@users.noreply.github.com>
2023-02-20 18:55:31 +0530

    business hosting ongoing

---
+++ src/Components/ModalComponents/WhHighPerformanceAndResource.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const WhHighPerformanceAndResource = () => {
+  return (
+    <div className="space-y-8">
+      <div className="flex space-x-5 xl:space-x-10">
+        {/* <div>
+            <DomainGlobeIcon className="w-28 h-28 fill-text" />
+          </div> */}
+        <p className="text-text text-base lg:leading-7">
+          If you're running an online business, you need a reliable web hosting solution to keep up with your needs like SiteRacks. Our High-performance business web hosting is designed to give you the resources you need to drive your online business to success.
+        </p>
+      </div>
+
+      <p className="text-text text-base lg:leading-7">
+        One of the key features of business web hosting is unmetered traffic. This means you can have as many visitors to your site as you like without worrying about running out of bandwidth or data. This is essential if you're running e-commerce or other high-traffic sites.
+      </p>
+
+      <p className="text-text text-base lg:leading-7">
+        In addition, business web hosting typically comes with unlimited databases, which is essential if you have a lot of databases that need to be stored and accessed. This allows you to scale your business without worrying about hitting any limits.
+      </p>
+
+      <p className="text-text text-base lg:leading-7">Unlimited SSL is another crucial feature of business web hosting. SSL is essential for online security, and having unlimited SSL certificates means you can secure all your domains and subdomains without worrying about additional costs.</p>
+
+      <p className="text-text text-base lg:leading-7">
+        With unlimited email, you can create as many email accounts as you need, which is essential for any business. This allows you to create professional email addresses for all your team members and ensures you can communicate effectively with your customers.
+      </p>
+
+      <p className="text-text text-base lg:leading-7">These are just a few of the many features that come with high-performance business web hosting. With the right hosting solution, you can drive your online business to success and achieve your goals.</p>
+    </div>
+  );
+};
+
+export default WhHighPerformanceAndResource;
--- src/Components/Pages/FeatureCard/FCHighPerformanceResources.tsx
+++ src/Components/Pages/FeatureCard/FCHighPerformanceResources.tsx
@@ -1,11 +1,30 @@
+import PlainButton from "@/Components/Buttons/PlainButton";
+import ColumnTitleAndDesc from "@/Components/ColumnTitleAndDesc/ColumnTitleAndDesc";
+import WhHighPerformanceAndResource from "@/Components/ModalComponents/WhHighPerformanceAndResource";
 import { useModalContext } from "@/Context/ModalProvider";
 import React from "react";
+import HighPerformanceResources from "../../../../public/images/article/high-performance-resources.svg";
 
 const FCHighPerformanceResources = () => {
   const { requestModal, setModalData, setModalTitle } = useModalContext();
   return (
     <>
-      <article className="flex flex-col space-y-8 group items-start">High-performance resources</article>
+      <article className="flex flex-col space-y-8 group items-start">
+        <div className="w-full">
+          <HighPerformanceResources className="w-full h-fit max-h-[200px] lg:max-h-[240px]  xl:max-h-[340px]" />{" "}
+        </div>
+
+        <ColumnTitleAndDesc title="High-performance resources">
+          <p className="text-text lg:text-xl lg:leading-9">Drive your successful online business with high-performance business web hosting: unmetered traffic﻿, unlimited databases, unlimited SSL, unlimited email, and many more.</p>
+        </ColumnTitleAndDesc>
+
+        <PlainButton
+          text="LEARN MORE"
+          handler={() => {
+            setModalTitle("Premium Cloud Servers"), setModalData(<WhHighPerformanceAndResource />), requestModal();
+          }}
+        />
+      </article>
     </>
   );
 };
