63cc5d86cb4d46fc11492063ff5e6f0cdbe9b5f7
Sandipan Das <87384281+sandidas@users.noreply.github.com>
2023-02-23 00:22:33 +0530

    tiny edit

--- src/Components/Home/HomeArticle.tsx
+++ src/Components/Home/HomeArticle.tsx
@@ -10,7 +10,7 @@ const HomeArticle = () => {
   return (
     <section className="py-[10vh]">
       <div className="max-w-screen-2xl mx-auto px-3 md:px-5">
-        <SectionTitle title="a High-Performance Hosting Platform" subTitle="Dedicated to eCommerce Stores of Every Size" bottomSpace={true} />
+        <SectionTitle title="A High-Performance Hosting Platform" subTitle="That is Dedicated to Every Size of your website" bottomSpace={true} />
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 xl:gap-16 2xl:gap-32">
           <FCFreeWebsiteMigration />
           <FCPremiumCloudServers />
