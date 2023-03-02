8bdc04b4ff52fdeac16eeceddd4921a2bfe11f0a
Sandipan Das <87384281+sandidas@users.noreply.github.com>
2023-03-02 23:03:47 +0530

    all page loader added

--- src/Components/Loader/Loader.tsx
+++ src/Components/Loader/Loader.tsx
@@ -3,12 +3,11 @@ import IconSiteRacks from "../Header/IconSiteRacks";
 
 const Loader = () => {
   return (
-    <section className="w-full fixed top-0 bottom-0 left-0 h-full dark:bg-black/50 z-[999999] flex justify-center items-center">
+    <section className="w-full fixed top-0 bottom-0 left-0 h-full bg-white/80 dark:bg-black/90 z-[999999] flex justify-center items-center">
       <div className="flex flex-col justify-center items-center space-y-6">
         <div className="animate-pulse hover:animate-bounce">
-          <IconSiteRacks logoHeight="200px" logoWidth="200px" />
+          <IconSiteRacks logoHeight="100px" logoWidth="100px" />
         </div>
-        <h2>Welcome to the SiteRacks | Loading ....</h2>
       </div>
     </section>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import type { AppProps } from "next/app";
 import Layout from "@/Components/Layout/Layout";
 import "@/styles/globals.css";
-import type { ReactElement, ReactNode } from "react";
+import { ReactElement, ReactNode, useEffect, useState } from "react";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import Loader from "@/Components/Loader/Loader";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -13,11 +15,25 @@ type AppPropsWithLayout = AppProps & {
 };
 
 export default function App({ Component, pageProps }: AppProps) {
-  //
-  // const getLayout = Component.getLayout ?? ((page: ReactElement) => page);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = () => setLoading(true);
+    const handleComplete = () => setLoading(false);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+    };
+  }, [router]);
 
   return (
     <Layout>
+      {loading && <Loader />}
       <Component {...pageProps} />
     </Layout>
   );
