1163d4a250833b5510d8dc699fdf50d6c5c9ab39
Sandipan Das <87384281+sandidas@users.noreply.github.com>
2023-03-12 00:27:04 +0530

    brand assets ongoing

---
+++ src/pages/brand-assets/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import React from "react";
+
+const index = () => {
+  return (
+    <>
+      <Head>
+        <title>Brand Assets | SiteRacks</title>
+        <meta name="description" content="SiteRacks" />
+      </Head>
+      <main>
+        
+      </main>
+    </>
+  );
+};
+
+export default index;
