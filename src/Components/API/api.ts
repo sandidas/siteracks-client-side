b1bd3dc4c5185fa9eebed4ac1e14618c9337c137
Sandipan Das <87384281+sandidas@users.noreply.github.com>
2023-03-12 21:05:40 +0530

    api updated

--- src/Components/API/api.ts
+++ src/Components/API/api.ts
@@ -1,26 +1,26 @@
-import { useState, useEffect } from 'react';
-import axios, { AxiosResponse } from 'axios';
+// import { useState, useEffect } from 'react';
+// import axios, { AxiosResponse } from 'axios';
 
-interface Page {
-  _id: string;
-  pageSlug: string;
-}
+// interface Page {
+//   _id: string;
+//   pageSlug: string;
+// }
 
-const usePage = (slug: string): [Page | null, () => Promise<void>] => {
-  const [page, setPage] = useState<Page | null>(null);
+// const usePage = (slug: string): [Page | null, () => Promise<void>] => {
+//   const [page, setPage] = useState<Page | null>(null);
 
-  const fetchPage = async () => {
-    const response: AxiosResponse<Page[]> = await axios.get('http://localhost:3000/json/seo.json');
-    const pages = response.data;
-    const page = pages.find(p => p.pageSlug === slug) || null;
-    setPage(page);
-  };
+//   const fetchPage = async () => {
+//     const response: AxiosResponse<Page[]> = await axios.get('http://localhost:3000/json/seo.json');
+//     const pages = response.data;
+//     const page = pages.find(p => p.pageSlug === slug) || null;
+//     setPage(page);
+//   };
 
-  useEffect(() => {
-    fetchPage();
-  }, [slug]);
+//   useEffect(() => {
+//     fetchPage();
+//   }, [slug]);
 
-  return [page, fetchPage];
-};
+//   return [page, fetchPage];
+// };
 
-export default usePage;
+// export default usePage;
