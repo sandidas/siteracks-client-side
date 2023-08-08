23268cb62e64f5561862d56053124991f0e77cd8
Sandipan Das <87384281+sandidas@users.noreply.github.com>
2023-08-08 21:09:52 +0530

    GTag Added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,11 @@ import "@/styles/globals.css";
 import { ReactElement, ReactNode, useEffect, useState, useRef } from "react";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
-import LoaderComponent from "@/Components/Loader/LoaderComponent";
 import { toast, ToastBar, Toaster } from "react-hot-toast";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Progress } from "@mantine/core";
+import Script from "next/script";
 // https://www.youtube.com/watch?v=0ofxWRzBLWY
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
@@ -82,40 +82,58 @@ export default function App({ Component, pageProps }: AppProps) {
   }, [router]);
 
   return (
-    <Layout>
-      {/* Loader  */}
-      {/* {loading && <LoaderComponent />} */}
-      {loading && <Progress  size="sm" className="fixed z-auto left-0 w-full top-16" color="green" value={progressValue} />}
-      {/* by wrapping the component tree with the QueryClientProvider, you're setting up the queryClient object, which provides a centralized data management system and caching mechanism for all the child components that use the react-query library. */}
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
+    <>
+      <Layout>
+        {/* Loader  */}
+        {/* {loading && <LoaderComponent />} */}
+        {loading && <Progress size="sm" className="fixed z-auto left-0 w-full top-16" color="green" value={progressValue} />}
+        {/* by wrapping the component tree with the QueryClientProvider, you're setting up the queryClient object, which provides a centralized data management system and caching mechanism for all the child components that use the react-query library. */}
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
 
-      <Toaster
-        reverseOrder={false}
-        position="top-right"
-        toastOptions={{
-          className: "flex shadow shadow-primary gap-6 rounded-lg overflow-hidden max-w-sm dark:bg-gray-700 dark:text-white divide-gray-700 text-sm p-0 pl-2",
-          duration: 5000,
-          style: {},
+        <Toaster
+          reverseOrder={false}
+          position="top-right"
+          toastOptions={{
+            className: "flex shadow shadow-primary gap-6 rounded-lg overflow-hidden max-w-sm dark:bg-gray-700 dark:text-white divide-gray-700 text-sm p-0 pl-2",
+            duration: 5000,
+            style: {},
+          }}
+        >
+          {(t) => (
+            <ToastBar toast={t}>
+              {({ icon, message }) => (
+                <>
+                  {icon}
+                  {message}
+                  {t.type !== "loading" && (
+                    <button className="bg-surface dark:hover:bg-red-600 hover:bg-primary group h-full w-2/6 max-w-[40px] flex justify-center items-center" onClick={() => toast.dismiss(t.id)}>
+                      <XMarkIcon className="w-5 h-5 dark:fill-white fill-primary group-hover:fill-white" />
+                    </button>
+                  )}
+                </>
+              )}
+            </ToastBar>
+          )}
+        </Toaster>
+      </Layout>
+
+      <Script async strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=UA-137252196-1" />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-TNM362WZZ7', {
+      page_path: window.location.pathname,
+    });
+  `,
         }}
-      >
-        {(t) => (
-          <ToastBar toast={t}>
-            {({ icon, message }) => (
-              <>
-                {icon}
-                {message}
-                {t.type !== "loading" && (
-                  <button className="bg-surface dark:hover:bg-red-600 hover:bg-primary group h-full w-2/6 max-w-[40px] flex justify-center items-center" onClick={() => toast.dismiss(t.id)}>
-                    <XMarkIcon className="w-5 h-5 dark:fill-white fill-primary group-hover:fill-white" />
-                  </button>
-                )}
-              </>
-            )}
-          </ToastBar>
-        )}
-      </Toaster>
-    </Layout>
+      />
+    </>
   );
 }
